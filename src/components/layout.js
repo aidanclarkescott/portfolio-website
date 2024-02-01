e5dd31d84455f37d5285d94f10f1a9c5a893f10c
aidanclarkescott <clarkescotta@gmail.com>
2024-02-01 03:34:22 -0800

    Removed body for Gatsby to auto-generate, modified outer structure and styling.

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,17 +2,7 @@ import * as React from "react"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  return (
-    <body
-      style={{
-        margin: `0 auto`,
-        // maxWidth: `var(--size-content)`,
-        padding: `var(--size-gutter)`,
-      }}
-    >
-      {children}
-    </body>
-  )
+  return <div className="layout-container">{children}</div>
 }
 
 export default Layout
